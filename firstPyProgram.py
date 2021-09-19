0fd1adcfa12fd161a60fcf4011f1440e3c5e7598
DarthKnight456 <prajwal.pr2008@gmail.com>
2021-09-19 18:52:53 -0500

    Idk

--- firstPyProgram.py
+++ firstPyProgram.py
@@ -1,3 +1,10 @@
 import sys
 
 print ("I was here")
+
+# Ask people how much money they have
+
+money = int(input ("How much money do you have?"))
+money_2 = int(input ("How much money do you make every year?"))
+total_amount = money + (money_2 * 10)
+print  ("You will have", total_amount, "in 10 years.")
