a1c0069480a34ea6da484b8c2346f9f370cf9c52
DarthKnight456 <prajwal.pr2008@gmail.com>
2021-09-19 19:00:44 -0500

    Added check for amount entered

--- firstPyProgram.py
+++ firstPyProgram.py
@@ -6,5 +6,10 @@ print ("I was here")
 
 money = int(input ("How much money do you have?"))
 money_2 = int(input ("How much money do you make every year?"))
+
+if money < 1 or money_2 < 1:
+    print ("Why are giving me false data ?")
+    sys.exit(1)
 total_amount = money + (money_2 * 10)
 print  ("You will have", total_amount, "in 10 years.")
+
